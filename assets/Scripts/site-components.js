51b75d46fa416d85df81559ef27799db5640a947
Sarat Kumar Rout <saratrout39@gmail.com>
2026-02-12 19:26:03 +0530

    Update site-components.js

--- assets/Scripts/site-components.js
+++ assets/Scripts/site-components.js
@@ -167,3 +167,23 @@ if (document.readyState === 'loading') {
   // DOM is already loaded
   SiteComponents.init();
 }
+// ===== Go to Top Button =====
+// Get the button
+let mybutton = document.getElementById("goTopBtn");
+
+// When the user scrolls down 20px from the top, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
